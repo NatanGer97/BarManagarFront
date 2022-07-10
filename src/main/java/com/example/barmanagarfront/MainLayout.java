0fb26a8a7e8e2690c401d01228a1283b2c2c146e
Alzoke <barkanalexi@gmail.com>
2022-07-10 15:33:24 +0300

    Added InventoryManagementView tab

--- src/main/java/com/example/barmanagarfront/MainLayout.java
+++ src/main/java/com/example/barmanagarfront/MainLayout.java
@@ -52,7 +52,6 @@ public class MainLayout extends AppLayout implements IInventoryObserver
     {
         Tabs tabs = new Tabs();
         tabs.add(createTab(VaadinIcon.HOME, "Home", MainLayout.class));
-        tabs.add(createTab(VaadinIcon.LINES_LIST, "Categories", CategorisView.class ));
         tabs.add(createTab(VaadinIcon.CART_O, "Added", CartView.class ));
         tabs.add(createTab(VaadinIcon.GLASS, "Bar Inventory", InventoryView.class ));
         tabs.add(createTab(VaadinIcon.DENTAL_CHAIR, "Bar Seats", SeatView.class ));
